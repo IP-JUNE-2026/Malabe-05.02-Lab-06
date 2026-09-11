a4b777d8365a4c848e2d50cf5e7df063bc5c86c7
IT26101251 <it26101251@my.sliit.lk>
2026-09-11 11:26:31 +0530

    Add IT26101251LAb6Q2A , IT26101251LAb6Q2B ,IT26101251LAb6Q2C to Q2

---
+++ Q2/IT26101251Lab6Q2A.java
@@ -0,0 +1,12 @@
+public class IT26101251Lab6Q2A {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        
+        System.out.println(); // Print a newline at the end for clean formatting
+    }
+}
